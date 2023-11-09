33900b93203e9b1418205f95a515b26f869dbed0
VitorBertoldi <117694686+VitorBertoldi@users.noreply.github.com>
2023-11-09 16:40:04 -0300

    primeiro commit

---
+++ CalculoMedia/src/Main.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        double nota1 = 3.2;
+        double nota2 = 7.8;
+        double nota3 = 8.6;
+        double nota4 = 9.4;
+
+        double media = (nota1 + nota2 + + nota3 + nota4) / 4;
+        System.out.println(media);
+    }
+}
